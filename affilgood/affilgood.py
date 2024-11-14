c51434304deeae85044f3e3a184c067c9a1c4f72
siris <nicolau.duransilva@sirisacademic.com>
2024-11-14 17:14:27 +0100

    feat: add geographical metadata normalisation from regionalisation scripts

--- affilgood/affilgood.py
+++ affilgood/affilgood.py
@@ -2,22 +2,22 @@
 from affilgood.span_identification.span_identifier import SpanIdentifier
 from affilgood.ner.ner import NER
 from affilgood.entity_linking.entity_linker import EntityLinker
-from affilgood.metadata_normalization.normalizer import Normalizer
+from affilgood.metadata_normalization.normalizer import GeoNormalizer
 
 class AffilGood:
     def __init__(self, span_model_path='nicolauduran45/affilgood-span-v2', ner_model_path='nicolauduran45/affilgood-ner-multilingual-v2', linker_model='default', metadata_normalization=True, device="cpu"):
         self.span_identifier = SpanIdentifier(model_path=span_model_path, device=device)
         self.ner = NER(model_path=ner_model_path, device=device)
         #self.entity_linker = EntityLinker(model=linker_model, device=device)
-        #self.normalizer = Normalizer()
+        self.normalizer = GeoNormalizer()
 
     def process(self, text):
         """Executes all steps: span identification, NER, entity linking, and normalization."""
         spans = self.get_span(text)
         entities = self.get_ner(spans)
         #linked_entities = self.get_entity_linking(entities)
-        #normalized_data = self.get_normalization(linked_entities)
-        return entities
+        normalized_data = self.get_normalization(entities)
+        return normalized_data
 
     def get_span(self, text):
         """Identifies spans within the input text."""
@@ -31,6 +31,10 @@ class AffilGood:
         """Links recognized entities to identifiers."""
         return self.entity_linker.link_entities(entities)
 
-    def get_normalization(self, linked_entities):
+    def get_normalization(self, entities):
         """Normalizes the linked entity metadata."""
-        return self.normalizer.normalize_metadata(linked_entities)
+        return self.normalizer.normalize(entities)
+    
+    def normalize_country(self, country):
+        """Normalizes the linked entity metadata."""
+        return self.normalizer.normalize_country(country)
